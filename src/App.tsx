e486004d3e55478b0ae8fecca8fea2d3acfa0003
Kabir Kashif <kashif.kabir60@gmail.com>
2025-03-03 15:45:49 +0500

    completed section 25

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles"
+import Button from './ui/Button'
+import Input from "./ui/Input"
+import Heading from "./ui/Heading"
 
-function App() {
-  const [count, setCount] = useState(0)
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`
 
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Subheading</Heading>
+        <Button onClick={ () => alert("Bye")}>
+          Hello
+        </Button>
+        <Input />
+      </StyledApp>
     </>
   )
 }
-
-export default App
