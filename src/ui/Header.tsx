bb5d60d253cdefdfa1d9d653ed43f7422072096c
Kabir Kashif <kashif.kabir60@gmail.com>
2025-12-31 13:54:41 +0500

    added header menu

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import Logout from "../features/authentication/Logout"
+import HeaderMenu from "./HeaderMenu"
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
@@ -12,7 +12,7 @@ export default function Header() {
 
   return (
     <StyledHeader>
-      <Logout />
+      <HeaderMenu />
     </StyledHeader>
   )
 }
